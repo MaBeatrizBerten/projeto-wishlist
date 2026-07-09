e310c1b73bc709ddcc529239c75c081fe544aa95
Maria Beatriz Berten de Moraes <160761976+MaBeatrizBerten@users.noreply.github.com>
2026-07-09 11:22:33 -0300

    Delete src directory

--- src/main/java/com/mariabeatriz/wishlist/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mariabeatriz.wishlist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
